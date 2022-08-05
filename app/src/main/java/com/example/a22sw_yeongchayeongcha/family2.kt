cdd402418dd5cc19b2c9068fb12dc06db5a4ac21
jinahan_bb <89175998+bbokah@users.noreply.github.com>
2022-08-05 15:13:13 +0900

    Good
    family

---
+++ app/src/main/java/com/example/a22sw_yeongchayeongcha/family2.kt
@@ -0,0 +1,11 @@
+package com.example.a22sw_yeongchayeongcha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class family2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_family2)
+    }
+}
---
+++ app/src/main/java/com/example/a22sw_yeongchayeongcha/familycheck.kt
@@ -0,0 +1,11 @@
+package com.example.a22sw_yeongchayeongcha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class familycheck : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_familycheck)
+    }
+}
